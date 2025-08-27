3746d42b95eef4484885bba28df4aba2bd288469
MARUNGs <mi1764@naver.com>
2025-08-27 11:42:18 +0900

    [SET] prisma 설정, docker와 연결 #1

---
+++ prisma/migrations/20250827021351_/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "password" VARCHAR(300) NOT NULL,
+    "nick_nm" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "remark_txt" VARCHAR(500) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."todos" (
+    "no" SERIAL NOT NULL,
+    "title" VARCHAR(50) NOT NULL,
+    "status" VARCHAR(2) NOT NULL,
+    "highlightYn" VARCHAR(1) NOT NULL,
+    "memo" TEXT NOT NULL,
+    "remark_txt" VARCHAR(500) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "userId" VARCHAR(300) NOT NULL,
+
+    CONSTRAINT "todos_pkey" PRIMARY KEY ("no")
+);
+
+-- CreateTable
+CREATE TABLE "public"."common_cd" (
+    "no" SERIAL NOT NULL,
+    "group_nm" VARCHAR(50) NOT NULL,
+    "code" VARCHAR(50) NOT NULL,
+    "value" VARCHAR(100) NOT NULL,
+    "remark_txt" VARCHAR(500) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "common_cd_pkey" PRIMARY KEY ("no")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nick_nm_key" ON "public"."User"("nick_nm");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."todos" ADD CONSTRAINT "todos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
